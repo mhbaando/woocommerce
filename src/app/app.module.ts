ba06899143d387694a0aaad8a578904abce1662c
mhbaando <maxamuudyare21@gmail.com>
2021-12-26 15:32:24 +0300

    Added Feature Section, Added Comunity Section and fixed some minor bugs on the navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeroSectionComponent } from './home/hero-section/hero-section.component';
+import { FeaturesComponent } from './home/features/features.component';
+import { CommunityComponent } from './home/community/community.component';
+import { TestimonialsComponent } from './home/testimonials/testimonials.component';
 
 @NgModule({
   declarations: [
@@ -12,6 +15,9 @@ import { HeroSectionComponent } from './home/hero-section/hero-section.component
     HomeComponent,
     NavbarComponent,
     HeroSectionComponent,
+    FeaturesComponent,
+    CommunityComponent,
+    TestimonialsComponent,
   ],
   imports: [BrowserModule],
   providers: [],
